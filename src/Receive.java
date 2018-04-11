1b3b0e35af2a1ee8d1810ee247af8e793fe24759
Michael Schreiber <schreiber.gausburg@web.de>
2018-04-11 18:46:21 +0200

    refactoring stuff

--- src/Receive.java
+++ src/Receive.java
@@ -14,19 +14,19 @@ public class Receive {
 	 */
 	public static void main(String[] args) {
 		// check args
-        if(args.length != 1) {
-            System.out.println("> Java [Receive] -> argument(s): <port>");
-            return;
-        }
-        
-        // arg[0] -> port
-        int port = Integer.valueOf(args[0]);
+		if(args.length != 1) {
+			System.out.println("> Java [Receive] -> argument(s): <port>");
+			return;
+		}
 		
-        // count received packets
+		// arg[0] -> port
+		int port = Integer.valueOf(args[0]);
+		
+		// count received packets
 		int countReceivedPackets = 0;
 		
 		// lookup table for lost / duplicated packets
-        byte lookupTable[][] = new byte[10000][1500];
+		byte lookupTable[][] = new byte[10000][1500];
 		
 		// start time
 		long startTime = 0;
@@ -42,74 +42,72 @@ public class Receive {
 			socket = new DatagramSocket(port);
 			
 			// buffer
-            byte[] buffer = new byte[65536];
-            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
-            
-            System.out.println("> Java [Receive] -> socket created - listening on port " + port + " for incoming data...");
+			byte[] buffer = new byte[65536];
+			DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
 			
-            while (true) {
-            	// receive incoming data
-                socket.receive(incoming);
-                
-                // receive data
-                byte[] incomingData = incoming.getData();
-                // ... convert to byte
-                int[] data = utilByteToInt(incomingData);
-                
-                System.out.println("> Java [Receive] -> data received - value(s): [0] " + data[0] + " [1] " + data[1]);
-                
-                // transmit
-                if(data[1] != 0) {
-                	if (!started) {
-                		startTime = System.nanoTime();
-                        started = true;
-                	}
-                	
-                	++countReceivedPackets;
-                	
-                	//System.out.println("> packet " + countReceivedPackets + " " + incomingData + " " + data);
-                	
-                	// increase the count of the packet with that sequence number
-                    lookupTable[data[0]] = incomingData;
-                }
-                // end transmit
-                else if (countReceivedPackets > 0 && data[1] == 0) {
-                	
-                	// evaluate stuff...
-                	double endTime = (System.nanoTime() - startTime) / 1000000.0;
-                	int duplicatePackages = countReceivedPackets - data[0];
-                	int lostPackages = 0;
+			System.out.println("> Java [Receive] -> socket created - listening on port " + port + " for incoming data...");
+			
+			while (true) {
+				// receive incoming data
+				socket.receive(incoming);
+				
+				// receive data
+				byte[] incomingData = incoming.getData();
+				// ... convert to byte
+				int[] data = decodeDataToParameters(incomingData);
+				
+				System.out.println("> Java [Receive] -> data received - value(s): [0] " + data[0] + " [1] " + data[1]);
+				
+				// transmit
+				if(data[1] != 0) {
+					if (!started) {
+						startTime = System.nanoTime();
+						started = true;
+					}
+					
+					++countReceivedPackets;
+					
+					// increase the count of the packet with that sequence number
+					lookupTable[data[0]] = incomingData;
+				}
+				// end transmit
+				else if (countReceivedPackets > 0 && data[1] == 0) {
+					
+					// evaluate stuff...
+					double endTime = (System.nanoTime() - startTime) / 1000000.0;
+					int duplicatePackages = countReceivedPackets - data[0];
+					int lostPackages = 0;
 					for(int i = 0; i < data[0]; ++i) {
 						if(lookupTable[i] == null) {
 							++lostPackages;
 						}
 					}
-                	
-					// TODO: write to file?
-                	System.out.println("> Java [Receive] -> finished transmitting - endTime: " + endTime + " lostPackages: " + lostPackages + " countReceivedPackets: " + countReceivedPackets + " duplicatePackages: " + duplicatePackages);
 					
-                	// reset stuff...
-                	started = false;
-                	countReceivedPackets = 0;
-                	lookupTable = new byte[10000][1500];
-                }
-            }
-            
+					// TODO: write to file?
+					System.out.println("> Java [Receive] -> finished transmitting - endTime: " + endTime + " lostPackages: " + lostPackages + " countReceivedPackets: " + countReceivedPackets + " duplicatePackages: " + duplicatePackages);
+					
+					// reset stuff...
+					started = false;
+					countReceivedPackets = 0;
+					lookupTable = new byte[10000][1500];
+				}
+			}
+			
 		} catch (IOException e) {
-            System.err.println("IOException " + e);
-        }
+			System.err.println("IOException " + e);
+		}
 	}
 	
 	/**
-	 * Convert bytes to integers
+	 * Decode data bytes to parameters
 	 * 
 	 * @param b
 	 * @return
 	 */
-	private static int[] utilByteToInt(byte[] b) {
-		int packet = (b[0] << 24) & 0xff000000 | (b[1] << 16) & 0x00ff0000 | (b[2] << 8) & 0x0000ff00 | (b[3] << 0) & 0x000000ff;
-		int packets = (b[4] << 24) & 0xff000000 | (b[5] << 16) & 0x00ff0000 | (b[6] << 8) & 0x0000ff00 | (b[7] << 0) & 0x000000ff;
-	    
-		return new int[] {packet, packets};
+	private static int[] decodeDataToParameters(byte[] data) {
+		int counter = (data[0] << 24) & 0xff000000 | (data[1] << 16) & 0x00ff0000 | (data[2] << 8) & 0x0000ff00 | (data[3] << 0) & 0x000000ff;
+		int packets = (data[4] << 24) & 0xff000000 | (data[5] << 16) & 0x00ff0000 | (data[6] << 8) & 0x0000ff00 | (data[7] << 0) & 0x000000ff;
+		
+		return new int[] {counter, packets};
 	}
 }
--- src/Transmit.java
+++ src/Transmit.java
@@ -14,102 +14,100 @@ public class Transmit {
 	 */
 	public static void main(String[] args) {
 		// check args
-        if(args.length != 3) {
-            System.out.println("> Java [Transmit] -> argument(s): <ip> <port> <packets>");
-            return;
-        }
-        
-        // arg[0] -> internet protocoll
-        InetAddress ip;
+		if(args.length != 4) {
+			System.out.println("> Java [Transmit] -> argument(s): <ip> <port> <packets> <size>");
+			return;
+		}
+		
+		// arg[0] -> internet protocoll
+		InetAddress ip;
 		try {
 			ip = InetAddress.getByName(args[0]);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 			return;
 		}
-        // arg[1] -> port
-        int port = Integer.valueOf(args[1]);
-        // arg[2] -> packaets
-        int packets = Integer.valueOf(args[2]);
-        
-        // int -> byte size
-        int size = 8;
-        
-        // packets sequence number
-        int counter = -1;
-        
-        // socket
-        DatagramSocket socket = null;
-        
-        try {
-        	// create socket
-        	socket = new DatagramSocket();
-            
-        	System.out.println("> Java [Transmit] -> socket created - ip: " + ip + " port: " + port);
-        	
-        	// data format:
-        	// [0] = current packet number (counter)
-        	// [1] = number of total packets (packets) to be transmitted OR 0 if its an exit packet
-        	byte[] data;
-        	DatagramPacket datagramPacket;
-        	
-        	// send normal packets:
-        	
+		// arg[1] -> port
+		int port = Integer.valueOf(args[1]);
+		// arg[2] -> packaets
+		int packets = Integer.valueOf(args[2]);
+		// arg[3] -> int to byte size (packet size)
+		int size = Integer.valueOf(args[3]);
+		
+		// packets sequence number
+		int counter = -1;
+		
+		// socket
+		DatagramSocket socket = null;
+		
+		try {
+			// create socket
+			socket = new DatagramSocket();
+			
+			System.out.println("> Java [Transmit] -> socket created - ip: " + ip + " port: " + port);
+			
+			// data format:
+			// [0] = current packet number (counter)
+			// [1] = number of total packets (packets) to be transmitted OR 0 if its an exit packet
+			byte[] data;
+			DatagramPacket datagramPacket;
+			
+			// send normal packets:
+			
 			while (counter != packets - 1) {
 				++counter;
 				
 				// set data
-				data = utilIntToByte(counter, packets, size);
+				data = encodeParametersToData(counter, packets, size);
 			
-			    // create the packet
-			    datagramPacket = new DatagramPacket(data, data.length, ip, port);
+				// create the packet
+				datagramPacket = new DatagramPacket(data, data.length, ip, port);
 			
-			    //send the message to receiver
-			    socket.send(datagramPacket);
-			    
-			    System.out.println("> Java [Transmit] -> sending packet " + counter);
+				//send the message to receiver
+				socket.send(datagramPacket);
+				
+				System.out.println("> Java [Transmit] -> sending packet " + counter);
 			}
-            
-            // send exit packets:
 			
-            data = utilIntToByte(packets, 0, 8);
-            datagramPacket = new DatagramPacket(data, data.length, ip, port);
+			// send exit packets:
+			
+			data = encodeParametersToData(packets, 0, 8);
+			datagramPacket = new DatagramPacket(data, data.length, ip, port);
 
-            for(int i = 0; i < 10; ++i) {
-            	socket.send(datagramPacket);
-            	System.out.println("> Java [Transmit] -> sending EXIT packet " + i);
-            }
-	        
-	        // shutdown:
-            
-            socket.close();
-            System.exit(0);
-            
-        } catch (IOException e) {
-            System.err.println("IOException " + e);
-        }
+			for(int i = 0; i < 10; ++i) {
+				socket.send(datagramPacket);
+				System.out.println("> Java [Transmit] -> sending EXIT packet " + i);
+			}
+			
+			// shutdown:
+			
+			socket.close();
+			System.exit(0);
+			
+		} catch (IOException e) {
+			System.err.println("IOException " + e);
+		}
 	}
 	
 	/**
-	 * Convert integer to bytes
+	 * Encode parameters to data bytes
 	 * 
-	 * @param packet
-	 * @param packets
-	 * @param size
+	 * @param counter	current packet index
+	 * @param packets	number of total packets
+	 * @param size		size of the encoded data
 	 * @return
 	 */
-	private static byte[] utilIntToByte(int packet, int packets, int size) {
-		byte[] result = new byte[size];
+	private static byte[] encodeParametersToData(int counter, int packets, int size) {
+		byte[] data = new byte[size];
+		data[0] = (byte) (counter >> 24);
+		data[1] = (byte) (counter >> 16);
+		data[2] = (byte) (counter >> 8);
+		data[3] = (byte) counter;
+		data[4] = (byte) (packets >> 24);
+		data[5] = (byte) (packets >> 16);
+		data[6] = (byte) (packets >> 8);
+		data[7] = (byte) packets;
 		
-		result[0] = (byte) (packet >> 24);
-		result[1] = (byte) (packet >> 16);
-		result[2] = (byte) (packet >> 8);
-		result[3] = (byte) packet;
-		result[4] = (byte) (packets >> 24);
-		result[5] = (byte) (packets >> 16);
-		result[6] = (byte) (packets >> 8);
-		result[7] = (byte) packets;
-		
-		return result;
+		return data;
 	}
 }
